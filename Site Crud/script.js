7ab71b6d566e060f37923269d4cc52ec8df7c65e
Antonio-Emerson <165593121+Antonio-Emerson@users.noreply.github.com>
2025-04-03 16:30:27 -0300

    Add files via upload

---
+++ Site Crud/script.js
@@ -0,0 +1,38 @@
+document.getElementById('registrationForm").addEventListener('submit',
+     function(event) {
+    event.preventDefault(); // Impede o envio do formulário por padrão
+    
+    const username = document.querySelector('input[name="username"]').value;
+    const email = document.querySelector('input[name="email"]").value; const password = document.querySelector('input[name="password"]').value;
+    let valid = true;
+    let message = ";
+    
+    // Validação do nome de usuário if (username.trim()") { message += 'O nome de usuário é obrigatório.\n'; }
+    
+    valid = false;
+    
+    // Validação do e-mail
+    
+    const emailPattern = /^[^\s@]+@[^s@]+\[^s@]+$/;
+    
+    if (!emailPattern.test(email)) { message += 'Por favor, insira um e-mail válido.\n';
+    
+    valid = false;
+    
+    // Validação da senha
+    
+    if (password.length <6) { message += 'A senha deve ter pelo menos 6 caracteres.\n'; }
+    
+    valid = false;
+    
+    if (valid) {
+    
+    alert("Cadastro realizado com sucesso!");
+    
+    this.submit(); // Envia o formulário se tudo estiver válido
+    
+    } else {
+    
+    alert(message); // Exibe mensagens de erro
+    
+    } });
