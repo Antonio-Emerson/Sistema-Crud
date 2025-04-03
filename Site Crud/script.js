8645a224f97d05c00f660b28882b1f02a544654c
Antonio-Emerson <165593121+Antonio-Emerson@users.noreply.github.com>
2025-04-03 16:44:12 -0300

    Update script.js

--- Site Crud/script.js
+++ Site Crud/script.js
@@ -3,36 +3,32 @@ document.getElementById('registrationForm").addEventListener('submit',
     event.preventDefault(); // Impede o envio do formulário por padrão
     
     const username = document.querySelector('input[name="username"]').value;
-    const email = document.querySelector('input[name="email"]").value; const password = document.querySelector('input[name="password"]').value;
+    const email = document.querySelector('input[name="email"]').value;
+     const password = document.querySelector('input[name="password"]').value;
     let valid = true;
-    let message = ";
+    let message = "";
     
-    // Validação do nome de usuário if (username.trim()") { message += 'O nome de usuário é obrigatório.\n'; }
-    
+    // Validação do nome de usuário 
+     if (username.trim() === "") {
     valid = false;
+      message += 'O nome de usuário é obrigatório.\n'; 
+}
     
     // Validação do e-mail
-    
-    const emailPattern = /^[^\s@]+@[^s@]+\[^s@]+$/;
-    
-    if (!emailPattern.test(email)) { message += 'Por favor, insira um e-mail válido.\n';
-    
+    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailPattern.test(email)) { 
     valid = false;
+     message += 'Por favor, insira um e-mail válido.\n';
+    }
     
-    // Validação da senha
-    
-    if (password.length <6) { message += 'A senha deve ter pelo menos 6 caracteres.\n'; }
-    
-    valid = false;
-    
+     // Validação da senha
+     if (password.length <6) { 
+      valid = false;
+   message += 'A senha deve ter pelo menos 6 caracteres.\n'; }
     if (valid) {
-    
-    alert("Cadastro realizado com sucesso!");
-    
-    this.submit(); // Envia o formulário se tudo estiver válido
-    
+     alert("Cadastro realizado com sucesso!");
+     this.submit(); // Envia o formulário se tudo estiver válido
     } else {
-    
     alert(message); // Exibe mensagens de erro
-    
-    } });
+    } 
+});
